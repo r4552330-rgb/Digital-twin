863cec08433230370b48a5d7313d24a478bf48cc
r4552330-rgb <r4552330@gmail.com>
2026-04-29 20:00:08 +0100

    Add timezone and panel tilt/azimuth to config

--- config.py
+++ config.py
@@ -1,35 +1,42 @@
-# config.yaml
 site:
   latitude: 33.6
   longitude: -7.6
   altitude: 56
   name: "Centrale PV Demo"
+  tz: "Africa/Casablanca"
+  tilt: 31
+  azimuth: 180
+
   n_panels: 12
   series_per_string: 6
   strings_per_mppt: 2
   n_mppt: 1
 
 panel:
-  pmp_stc: 330.0       # W
-  area_m2: 1.939        # m²
-  eta_stc: 0.170        # rendement STC
-  voc: 40.0             # V
-  isc: 9.0              # A
-  vmp: 33.0             # V
-  imp: 8.5              # A
-  gamma_pmp: -0.0040    # -0.4%/K
+  pmp_stc: 330.0
+  area_m2: 1.939
+  eta_stc: 0.170
+  voc: 40.0
+  isc: 9.0
+  vmp: 33.0
+  imp: 8.5
+  gamma_pmp: -0.0040
   beta_voc: -0.0034
   alpha_isc: 0.0005
-  noct: 45              # °C
+  noct: 45
 
 losses:
-  dc_total: 0.10        # pertes DC totales (fraction)
-  ac_efficiency: 0.96   # rendement onduleur (corrigé !)
+  dc_total: 0.10
+  ac_efficiency: 0.96
+  inverter_threshold_kw: 0.05
+
+inverter:
+  p_rated_kw: 4.0
 
 ems:
   mqtt_broker: "localhost"
   mqtt_port: 1883
   mqtt_topic_telemetry: "esp32/telemetry"
   mqtt_topic_command: "esp32/command"
-  blynk_api_key: ""     # optionnel, si vous gardez Blynk
+  blynk_api_key: ""
   open_meteo_interval_min: 15
